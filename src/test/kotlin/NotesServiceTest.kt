1d41729ee7c02946696a1d243e2f836a4ed0a79e
Katana Ruslan <ruskatanaa@mail.ru>
2023-04-09 22:06:11 +0300

    5 commit

--- src/test/kotlin/NotesServiceTest.kt
+++ src/test/kotlin/NotesServiceTest.kt
@@ -16,4 +16,117 @@ class NotesServiceTest {
         val result = NotesService.add(note)
         assertEquals(result, 1)
     }
+
+    @Test
+    fun createCommentSuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments(5, "комментарий")
+        NotesService.add(note)
+        val result = NotesService.createComment(1, comment)
+        assertEquals(result, 1)
+    }
+
+    @Test
+    fun createCommentUnsuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments(5, "комментарий")
+        NotesService.add(note)
+        val result = NotesService.createComment(205, comment)
+        assertEquals(result, -1)
+    }
+
+    @Test
+    fun deleteSuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        NotesService.add(note)
+        val result = NotesService.delete(note.id)
+        assertEquals(result, true)
+    }
+
+    @Test
+    fun deleteUnsuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val result = NotesService.delete(note.id)
+        assertEquals(result, false)
+    }
+
+    @Test
+    fun deleteCommentSuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments(5, "комментарий")
+        NotesService.add(note)
+        NotesService.createComment(1, comment)
+
+        val result = NotesService.deleteComment(comment.id)
+        assertEquals(result, true)
+    }
+
+    @Test
+    fun deleteCommentUnsuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments(5, "комментарий")
+        NotesService.add(note)
+        NotesService.createComment(1, comment)
+
+        val result = NotesService.deleteComment(5)
+        assertEquals(result, false)
+    }
+
+    @Test(expected = CommentAlreadyDeletedException::class)
+    fun shouldThrowDeleteComment() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments(5, "комментарий")
+        NotesService.add(note)
+        NotesService.createComment(1, comment)
+        NotesService.deleteComment(1)
+        NotesService.deleteComment(1)
+    }
+
+    @Test
+    fun edit() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        NotesService.add(note)
+        val result = NotesService.editNote(note, "текст", "заголовок")
+        assertEquals(result, true)
+    }
+
+    @Test
+    fun editComment() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments(5, "комментарий")
+        NotesService.add(note)
+        NotesService.createComment(1, comment)
+
+        val result = NotesService.editComment(comment, "сообщение")
+        assertEquals(result, true)
+    }
+    @Test
+    fun noteGet() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        NotesService.add(note)
+
+        val result = NotesService.noteGetById(1)
+        assertEquals(result, note)
+    }
+    @Test
+    fun getCommentsOfNoteSuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments (5, "комментарий")
+        NotesService.add(note)
+        NotesService.createComment(1, comment)
+
+        val result = NotesService.getCommentsOfNote(1)
+        assertEquals(result, note.notesComent)
+    }
+    @Test
+    fun restoreCommentSuccessful() {
+        val note = Notes(2, "заметка", "заметка заметканная")
+        val comment = NoteComments (2, "заметка")
+        NotesService.add(note)
+        NotesService.createComment(1, comment)
+        NotesService.deleteComment(comment.id)
+
+        val result = NotesService.restoreComment(comment.id)
+        assertEquals(result, true)
+    }
 }
